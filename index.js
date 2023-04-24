772dbf8dfc4b46e71aa22f05308ae1aee852ec7a
Kiran Kumar Machineni <kiran-machineni@users.noreply.github.com>
2023-04-24 01:31:47 +0000

    Add: /api/fileanalyse route

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+const multer = require("multer")
+const bodyParser = require("body-parser") // Add this line
+require("dotenv").config()
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: "uploads/" })
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
+app.use(bodyParser.urlencoded({ extended: true })) // Add this line
 
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html")
+})
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	const fileType = req.file.mimetype
+	const fileSize = req.file.size
+	const fileName = req.file.originalname
+	res.send({ name: fileName, type: fileType, size: fileSize })
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+	console.log("Your app is listening on port " + port)
+})
